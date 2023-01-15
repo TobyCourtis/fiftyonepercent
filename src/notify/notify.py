7ca19ad977015f132d26af4888342479cdb82fdc
tcourtis <toby.courtis@hotmail.com>
2023-01-15 21:48:58 +0000

    TC ✨ Added channel param for slack notifier

--- src/notify/notify.py
+++ src/notify/notify.py
@@ -9,17 +9,21 @@ def google_mini_notify(text):
     requests.get("http://192.168.86.39:5000/say", params={"text": text})
 
 
-def slack_notify(text):
+def slack_notify(text, channel='kingslanding'):
     global auth_url
     if not auth_url:
         with open(os.path.dirname(__file__) + "/../keys/slack-webhook.json") as f:
             auth_url = json.loads(f.read())["auth_url"]
-    slack_payload = {"channel": "#kingslanding",
+    slack_payload = {"channel": f"#{channel}",
                      "username": "dumbot",
                      "text": "<!here> {}".format(text),
                      "icon_emoji": ":slack:"}
     requests.post(f"https://hooks.slack.com/services/{auth_url}",
                   data=json.dumps(slack_payload))
 
+
 # google_mini_notify("stonks are through the roof right now!")
 # slack_notify("test")
+
+if __name__ == "__main__":
+    slack_notify("Bot test", "crypto-trading")
