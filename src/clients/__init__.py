e3d36c84bf28f8000e0685bd2b01b2c18c753a16
tcourtis <toby.courtis@hotmail.com>
2023-01-26 22:43:42 +0000

    TC ✨ Altered clients package to append it's path to sys.path for testing

--- src/clients/__init__.py
+++ src/clients/__init__.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))  # required for unittest to run
+
 from . import main
 from . import helpers
 from . import candlesticks
