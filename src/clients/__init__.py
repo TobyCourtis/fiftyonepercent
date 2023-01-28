d35a63f4a67ea7ab2d93c25441def92f35f92b05
tcourtis <toby.courtis@hotmail.com>
2023-01-28 00:15:29 +0000

    TC ✨ Added parent dir import so tests can execute

--- src/clients/__init__.py
+++ src/clients/__init__.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+
+root_path = str(Path(__file__).parent.parent.parent)
+if root_path not in sys.path:
+    sys.path.append(root_path)
+
 from . import binance_client
 from . import helpers
 from . import candlesticks
