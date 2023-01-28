f9cbfc17a3d70cfbf5d46128e54747fbc826e1b8
tcourtis <toby.courtis@hotmail.com>
2023-01-28 14:59:54 +0000

    TC ✨ Added file upload functionality to Slack v0.1

---
+++ src/notify/slack_image_upload.py
@@ -0,0 +1,31 @@
+import json
+import os
+
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+from src.notify import notifier
+
+
+# If you're using this in production, you can change this back to INFO and add extra log entries as needed.
+# logging.basicConfig(level=logging.INFO)
+
+
+def upload_plot():
+    try:
+        with open(os.path.dirname(__file__) + "/../keys/slack-notifier-bot-oauth.json") as f:
+            SLACK_BOT_TOKEN = json.loads(f.read())["SLACK_BOT_TOKEN"]
+
+        client = WebClient(SLACK_BOT_TOKEN)
+
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        file_dir = f"{current_dir}/../clients/plot.png"
+
+        new_file_res = client.files_upload(
+            channels="crypto-trading",
+            title="Current Plot",
+            file=file_dir,
+            initial_comment="Latest plot using {windowMin}, {windowMax}, units='days'",
+        )
+    except SlackApiError as e:
+        notifier.slack_notify(f"Current plot upload failed with exception: {e}")
